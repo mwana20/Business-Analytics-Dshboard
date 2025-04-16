2441c8c9fda32614685af41d7ead10508a1706f8
mwana20 <phillipmwanaweika@gmail.com>
2025-04-16 06:37:58 -0900

    pages

--- Business-Analytic-Dashboard/js/auth.js
+++ Business-Analytic-Dashboard/js/auth.js
@@ -4,6 +4,6 @@ import { navigateTo } from './utils.js';
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('start-analysis-btn').addEventListener('click', () => {
 
-    window.location.href = '.src/dashboard.html';
+    window.location.href = '../src/dashboard.html';
   });
 });
