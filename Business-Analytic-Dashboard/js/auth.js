28b6689b1623bd50ea0e71c86e1abfa3bf88894c
mwana20 <phillipmwanaweika@gmail.com>
2025-04-16 22:53:34 -0900

    hamburger

--- Business-Analytic-Dashboard/js/auth.js
+++ Business-Analytic-Dashboard/js/auth.js
@@ -1,9 +1,13 @@
-
 import { navigateTo } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('start-analysis-btn').addEventListener('click', () => {
-
     window.location.href = '../src/dashboard.html';
+  });
+
+  document.querySelectorAll('.menu ul li a').forEach(link => {
+    link.addEventListener('click', () => {
+      document.getElementById('menu-toggle').checked = false;
+    });
   });
 });
