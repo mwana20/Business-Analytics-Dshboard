4f85ab13805bbdc41fddd0e86a9da0ac64c6affb
mwana20 <phillipmwanaweika@gmail.com>
2025-04-16 22:13:34 -0900

    one

--- Business-Analytic-Dashboard/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './', // Set the root directory
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
-});
